18db3a8225fc0ce6c0ada3e3df1f73940e080f47
Илья <gromov@MacBook-Air-Ila.local>
2022-12-08 22:21:52 +0300

    add

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,6 +1,18 @@
+from gendiff.generate_diff import generate_diff
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("first_file")
 parser.add_argument("second_file")
 parser.add_argument("-f", "--format", help="set format for output")
-parser.parse_args()
+args = parser.parse_args()
+
+
+diff = generate_diff(args.first_file, args.second_file)
+
+
+def main():
+    print(diff)
+
+
+if __name__ == "__main__":
+    main()
