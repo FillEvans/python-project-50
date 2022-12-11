28983c7ffcb48606403210b99de89b4392186328
Илья <gromov@MacBook-Air-Ila.local>
2022-12-11 23:11:35 +0300

    fixing lint

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,28 +1,34 @@
 from gendiff.json_to_str import get_json_to_str
 
+
+def get_diff_if_same(data1, data2, diff, key):
+    if key in data1.keys() and key in data2.keys():
+        if data1[key] == data2[key]:
+            diff.append({
+                "key": key,
+                "value": data1[key],
+                "type": ' '
+            })
+        elif data1[key] != data2[key]:
+            diff.append({
+                "key": key,
+                "value": data1[key],
+                "type": '-'
+            })
+            diff.append({
+                "key": key,
+                "value": data2[key],
+                "type": '+'
+            })
+    return diff
+
+
 def get_diff(filepath1, filepath2):
     data1 = get_json_to_str(filepath1)
     data2 = get_json_to_str(filepath2)
     diff = []
     for key in sorted(set([*data1.keys(), *data2.keys()])):
-        if key in data1.keys() and key in data2.keys():
-            if data1[key] == data2[key]:
-                diff.append({
-                    "key": key,
-                    "value": data1[key],
-                    "type": ' '
-                })
-            elif data1[key] != data2[key]:
-                diff.append({
-                    "key": key,
-                    "value": data1[key],
-                    "type": '-'
-                })
-                diff.append({
-                    "key": key,
-                    "value": data2[key],
-                    "type": '+'
-                })
+        get_diff_if_same(data1, data2, diff, key)
         if key in data1.keys() and key not in data2.keys():
             diff.append({
                 "key": key,
