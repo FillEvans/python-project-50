4ad3915d73ce09714a9446e58dafa4169c2bde93
Илья <gromov@MacBook-Air-Ila.local>
2022-12-14 00:05:27 +0300

    test

--- gendiff/yml_to_str.py
+++ gendiff/yml_to_str.py
@@ -3,6 +3,3 @@ import yaml
 
 def get_yml_to_str(filepath):
     return yaml.safe_load(open(filepath))
-
-
-print(get_yml_to_str("/Users/gromov/Projects/python-project-50/tests/fixtures/file1_for_test.yaml"))
